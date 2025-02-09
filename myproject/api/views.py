0a1ab8618dc38daabec71c5764e43b425ab3039b
adrianapachecor <acpachecor@gmail.com>
2025-02-09 13:09:10 -0300

    primer commit

---
+++ myproject/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+
+
+@api_view(['GET'])
+def getData(request):
+    person ={'name':'Adriana', 'age':31}
+    return Response(person)
+
+
+
